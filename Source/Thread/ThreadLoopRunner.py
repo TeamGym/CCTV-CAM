497dd68bf2f2ca2f797e872c7db3ce80c149df94
ReolSt <roxman98@naver.com>
2022-04-19 06:02:27 +0900

    [UPDATE]: add ThreadLoopRunner feature

--- Source/Thread/ThreadLoopRunner.py
+++ Source/Thread/ThreadLoopRunner.py
@@ -1,24 +1,45 @@
+import time
 from threading import Thread
 
 class ThreadLoopRunner(Thread):
-    def __init__(self, func):
+    def __init__(self, func, minInterval=0):
         super().__init__()
 
         self.__func = func
-        self.__isRunning = True
+
+        self.__isRunning = False
+        self.__minInterval = minInterval
 
     @property
     def func(self):
         return self.__func
 
+    @property
     def isRunning(self):
         return self.__isRunning
 
-    def setRunning(self, value):
+    @isRunning.setter
+    def isRunning(self, value):
         assert isinstance(value, bool)
 
         self.__isRunning = value
 
+    @property
+    def minInterval(self):
+        return self.__minInterval
+
+    @minInterval.setter
+    def minInterval(self, value):
+        self.__minInterval = value
+
     def run(self):
-        while self.isRunning():
+        self.isRunning = True
+        while self.isRunning:
+            startTime = time.time()
+
             self.__func()
+
+            endTime = time.time()
+            elapsedTime = endTime - startTime
+
+            time.sleep(max(self.__minInterval - elapsedTime, 0))
