6a1a4183b28b11c31019b4b4ecb563cbd56e2082
ReolSt <roxman98@naver.com>
2022-04-27 01:56:51 +0900

    [REFACTOR]: refactor video writer class.

--- Source/Video/VideoWriter.py
+++ Source/Video/VideoWriter.py
@@ -6,7 +6,7 @@ class VideoWriter:
 
         self.__width = 0
         self.__height = 0
-        self.__frameRate = 0
+        self.__fps = 0
 
 # ----------------------------------------------------------------------
 # Property
@@ -21,34 +21,35 @@ class VideoWriter:
         return self.__height
 
     @property
-    def frameRate(self):
-        return self.__frameRate
+    def fps(self):
+        return self.__fps
 
 # ----------------------------------------------------------------------
 # Public Member Method
 # ----------------------------------------------------------------------
 
-    def openFile(self, fileName, fourccString, width, height, frameRate):
+    def openFile(self, fileName, fourcc, width, height, fps):
         self.__cvWriter = cv2.VideoWriter(
-            fileName, cv2.VideoWriter_fourcc(*fourccString), frameRate, (width, height))
+            fileName, cv2.VideoWriter_fourcc(*fourcc), fps, (width, height))
 
         self.__width = width
         self.__height = height
-        self.__frameRate = frameRate
+        self.__fps = fps
 
-    def openDevice(self, deviceName, fourccString, width, height, frameRate):
-        self.openFile(deviceName, fourccString, width, height, frameRate)
+    def openDevice(self, deviceName, fourcc, width, height, fps):
+        self.openFile(deviceName, fourcc, width, height, fps)
 
-    def openGstreamerPipeline(self, pipelineString, fourccString, width, height, frameRate):
+    def openGstreamerPipeline(self, pipelineString, fourcc, width, height, fps):
         self.__cvWriter = cv2.VideoWriter(
-            pipelineString, cv2.VideoWriter_fourcc(*fourccString), frameRate, (width, height))
+            pipelineString, cv2.VideoWriter_fourcc(*fourcc), fps, (width, height))
 
         self.__width = width
         self.__height = height
-        self.__frameRate = frameRate
+        self.__fps = fps
 
     def close(self):
-        self.__cvWriter.release()
+        if self.__cvWriter is not None:
+            self.__cvWriter.release()
 
     def write(self, image):
         self.__cvWriter.write(image)
