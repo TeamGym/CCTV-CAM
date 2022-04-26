f7fc1c71b9b3ba41c9a2d6a831256c0479bdd002
ReolSt <roxman98@naver.com>
2022-04-27 02:01:49 +0900

    [REMOVE]: remove remote device  controller temporarily.

--- Source/Network/RemoteDeviceController.py
+++
@@ -1,5 +0,0 @@
-class RemoteDeviceController:
-    def __init__(self, context):
-        self.__commandQueue = context.commandQueue
-        self.__audioDeviceController = context.audioDeviceController
-        self.__cameraDeviceController = context.cameraDeviceController
