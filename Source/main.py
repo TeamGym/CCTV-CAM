8d6e445b42f3f71abb74dc733b5a8e017c96323d
ReolSt <roxman98@naver.com>
2022-04-27 02:01:05 +0900

    [UPDATE]: reflect all changes to main.py

--- Source/main.py
+++ Source/main.py
@@ -2,7 +2,7 @@
 
 from Core import Buffer, BufferHolder, BufferBroadcaster, JSON
 from Thread import ThreadRunner, ThreadLoopRunner
-from Video import VideoCapture
+from Video import VideoCapture, ClipExporter
 from Audio import AudioDeviceController, TTSEngine, MotionAlerter, ObjectAlerter
 from Detect import MotionDetector, ObjectDetector
 from Network import ConnectionHolder, VideoStreamer, RemoteServerConnector
@@ -10,6 +10,7 @@ from Log import MotionLogger, ObjectLogger
 from Render import RenderableMonitor, BufferViewer
 
 import signal
+import os
 import sys
 import time
 import pyglet
@@ -29,7 +30,8 @@ bufferHolder.connectBuffers(
                 "Stream": Buffer(maxlen=500),
                 "Object": Buffer(maxlen=500),
                 "Motion": Buffer(maxlen=500),
-                "Render": Buffer(maxlen=100)
+                "Render": Buffer(maxlen=100),
+                "Record": Buffer()
             }),
         "ObjectOut": BufferBroadcaster(
             sockets={
@@ -60,17 +62,6 @@ videoCapture.openDevice(
     config.device.camera.height,
     config.device.camera.fps)
 
-motionDetector = MotionDetector(
-    config=config,
-    videoBuffer=bufferHolder.getBuffer("Video").getBuffer("Motion"),
-    outputBuffer=bufferHolder.getBuffer("MotionOut"),
-    renderBuffer=bufferHolder.getBuffer("MotionRender"))
-objectDetector = ObjectDetector(
-    config=config,
-    videoBuffer=bufferHolder.getBuffer("Video").getBuffer("Object"),
-    outputBuffer=bufferHolder.getBuffer("ObjectOut"),
-    renderBuffer=bufferHolder.getBuffer("ObjectRender"))
-
 monitors = [
     RenderableMonitor(name="Original",
                       buffer=bufferHolder.getBuffer("Video").getBuffer("Render"),
@@ -95,43 +86,72 @@ monitors = [
 ttsEngine = TTSEngine()
 ttsEngine.start()
 
-renderer = BufferViewer(30, monitors=monitors)
+renderer = BufferViewer(60, monitors=monitors)
+
+os.makedirs("../Log/" + time.strftime("%Y-%m-%d"), exist_ok=True)
 
 threads = [
     ThreadLoopRunner(
         func=videoCapture.read),
-    ThreadLoopRunner(
-        func=motionDetector.detect,
-        minInterval=1/config.detector.motion.targetFPS),
-    ThreadLoopRunner(
-        func=objectDetector.detect,
-        minInterval=1/config.detector.object.targetFPS),
+    ObjectDetector(
+        config=config.detector.object.config,
+        weights=config.detector.object.weights,
+        label=config.detector.object.label,
+        threshold=config.detector.object.threshold,
+        targetFPS=config.detector.object.targetFPS,
+        videoBuffer=bufferHolder.getBuffer("Video").getBuffer("Object"),
+        outputBuffer=bufferHolder.getBuffer("ObjectOut"),
+        renderBuffer=bufferHolder.getBuffer("ObjectRender")),
+    MotionDetector(
+        updateInterval=config.detector.motion.updateInterval,
+        updateThreshold=config.detector.motion.updateThreshold,
+        motionThreshold=config.detector.motion.motionThreshold,
+        targetFPS=config.detector.motion.targetFPS,
+        videoBuffer=bufferHolder.getBuffer("Video").getBuffer("Motion"),
+        outputBuffer=bufferHolder.getBuffer("MotionOut"),
+        renderBuffer=bufferHolder.getBuffer("MotionRender")),
+    ClipExporter(
+        prefix="../Clip/",
+        fourcc=config.video.clip.fourcc,
+        extension=config.video.clip.extension,
+        width=config.video.clip.width,
+        height=config.video.clip.height,
+        fps=config.video.clip.fps,
+        clipLength=config.video.clip.length,
+        videoBuffer=bufferHolder.getBuffer("Video").getBuffer("Record")),
     VideoStreamer(
-        config=config,
+        width=config.device.camera.width,
+        height=config.device.camera.height,
+        fps=config.device.camera.fps,
+        location=config.network.rtsp.location,
         connectionHolder=connectionHolder,
         videoBuffer=bufferHolder.getBuffer("Video").getBuffer("Stream")),
     RemoteServerConnector(
-        config=config,
+        host=config.network.tcp.host,
+        port=config.network.tcp.port,
+        videoWidth=config.device.camera.width,
+        videoHeight=config.device.camera.height,
         connectionHolder=connectionHolder,
         objectBuffer=bufferHolder.getBuffer("ObjectOut").getBuffer("Send"),
         motionBuffer=bufferHolder.getBuffer("MotionOut").getBuffer("Send"),
         commandQueue=bufferHolder.getBuffer("Command")),
     MotionAlerter(
-        config=config,
+        interval=config.audio.alert.motion.interval,
+        cooldown=config.audio.alert.motion.cooldown,
         detectionBuffer=bufferHolder.getBuffer("MotionOut").getBuffer("Alert"),
         engine=ttsEngine),
     ObjectAlerter(
-        config=config,
+        interval=config.audio.alert.object.interval,
+        targets=config.audio.alert.object.targets,
+        cooldown=config.audio.alert.object.cooldown,
         detectionBuffer=bufferHolder.getBuffer("ObjectOut").getBuffer("Alert"),
         engine=ttsEngine),
     MotionLogger(
-        filePath=time.strftime('../Log/Motion%Y-%m-%d%X', time.localtime()),
-        detectionBuffer=bufferHolder.getBuffer("MotionOut").getBuffer("Log")
-    ),
+        filePath=time.strftime('../Log/%Y-%m-%d/Motion_%H_%M_%S', time.localtime()),
+        detectionBuffer=bufferHolder.getBuffer("MotionOut").getBuffer("Log")),
     ObjectLogger(
-        filePath=time.strftime('../Log/Object%Y-%m-%d%X', time.localtime()),
-        detectionBuffer=bufferHolder.getBuffer("ObjectOut").getBuffer("Log")
-    )
+        filePath=time.strftime('../Log/%Y-%m-%d/Object_%H_%M_%S', time.localtime()),
+        detectionBuffer=bufferHolder.getBuffer("ObjectOut").getBuffer("Log"))
 ]
 
 for thread in threads:
